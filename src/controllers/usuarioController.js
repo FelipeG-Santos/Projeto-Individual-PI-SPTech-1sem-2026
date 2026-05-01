7445b2e9767351ed6e12975093bd158a4c16e5a2
FelipeG-Santos <felipe.msantos@sptech.school>
2026-05-01 14:33:16 -0700

    Alterando código base para inserir os dados de cadastro que irei utilizar

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -10,7 +10,7 @@ function autenticar(req, res) {
     var senha = req.body.senhaServer;
 
     if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
+        res.status(400).send("Seu email está indefinido!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está indefinida!");
     } else {
@@ -60,21 +60,34 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-    var fkEmpresa = req.body.idEmpresaVincularServer;
+
+
+    var estado = req.body.estadoServer;
+    var cidade = req.body.cidadeServer;
+    var bairro = req.body.bairroServer;
+
+    /* var fkEmpresa = req.body.idEmpresaVincularServer;*/
 
     // Faça as validações dos valores
     if (nome == undefined) {
-        res.status(400).send("Seu nome está undefined!");
+        res.status(400).send("Seu nome está indefinido!");
     } else if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
+        res.status(400).send("Seu email está indefinido!");
     } else if (senha == undefined) {
-        res.status(400).send("Sua senha está undefined!");
-    } else if (fkEmpresa == undefined) {
-        res.status(400).send("Sua empresa a vincular está undefined!");
+        res.status(400).send("Sua senha está indefinida!");
+        } else if (estado == undefined) {
+        res.status(400).send("Seu estado está indefinido!");
+    } else if (cidade == undefined) {
+        res.status(400).send("Sua cidade está indefinida!");
+        } else if (bairro == undefined) {
+        res.status(400).send("Seu bairro está indefinido!");
+    
+        /*} else if (fkEmpresa == undefined) {
+            res.status(400).send("Sua empresa a vincular está undefined!");*/
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
+        usuarioModel.cadastrar(nome, email, senha)
             .then(
                 function (resultado) {
                     res.json(resultado);
--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -6,7 +6,7 @@
 comandos para mysql server
 */
 
-CREATE DATABASE aquatech;
+/*CREATE DATABASE aquatech;
 
 USE aquatech;
 
@@ -60,3 +60,35 @@ insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B
 insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
 insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
 insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
+*/
+
+/*BANCO DE DADOS DO DIARIO DE UM BANANA*/
+create database DiarioDeUmBanana;
+use DiarioDeUmBanana;
+
+create table usuario (
+id_usuario int primary key auto_increment,
+nome varchar(45),
+email varchar(45) unique,
+senha varchar(45));
+
+create table endereco (
+id_endereco int primary key auto_increment,
+estado char(2),
+cidade varchar(45),
+bairro varchar(45),
+fk_usuario int,
+constraint ctfk_usuario foreign key (fk_usuario)
+references usuario (id_usuario)
+);
+
+create table avaliacao_livros (
+id_avaliacao_livros int primary key auto_increment,
+nome varchar(45),
+nota int,
+descricao varchar(100),
+fk_usuario int,
+constraint ctnota check(nota between 1 and 5),
+constraint ctfk_usuario_avaliacao foreign key (fk_usuario)
+references usuario (id_usuario)
+);
