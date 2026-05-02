9d8eb9eada15b6936890fb7aa28e506daa27beba
FelipeG-Santos <felipe.msantos@sptech.school>
2026-05-01 20:36:49 -0700

    Atualizando todos os dados e funções JS para cadastro e login adequados

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -3,7 +3,7 @@ autenticar,
     cadastrar */
 
 var usuarioModel = require("../models/usuarioModel");
-var aquarioModel = require("../models/aquarioModel");
+
 
 function autenticar(req, res) {
     var email = req.body.emailServer;
@@ -19,25 +19,18 @@ function autenticar(req, res) {
             .then(
                 function (resultadoAutenticar) {
                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
-                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+                   
 
                     if (resultadoAutenticar.length == 1) {
-                        console.log(resultadoAutenticar);
-
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
+                      
                                     res.json({
-                                        id: resultadoAutenticar[0].id,
+                                        id: resultadoAutenticar[0].id_usuario,
                                         email: resultadoAutenticar[0].email,
                                         nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha,
-                                        aquarios: resultadoAquarios
+                                        senha: resultadoAutenticar[0].senha
+                                        
                                     });
-                                } else {
-                                    res.status(204).json({ aquarios: [] });
-                                }
-                            })
+                               
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
@@ -66,7 +59,7 @@ function cadastrar(req, res) {
     var cidade = req.body.cidadeServer;
     var bairro = req.body.bairroServer;
 
-    /* var fkEmpresa = req.body.idEmpresaVincularServer;*/
+ 
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -82,12 +75,11 @@ function cadastrar(req, res) {
         } else if (bairro == undefined) {
         res.status(400).send("Seu bairro está indefinido!");
     
-        /*} else if (fkEmpresa == undefined) {
-            res.status(400).send("Sua empresa a vincular está undefined!");*/
+    
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome, email, senha, estado, cidade, bairro)
             .then(
                 function (resultado) {
                     res.json(resultado);
--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -1,26 +1,41 @@
 var database = require("../database/config")
 
+// autentica os dados do usuário permitindo o login
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
-    var instrucaoSql = `
-        SELECT id, nome, email, fk_empresa as empresaId FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    var instrucaoUsuario = `
+        SELECT id_usuario, nome, email FROM usuario WHERE email = '${email}' AND senha = '${senha}';
     `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+    console.log("Executando a instrução SQL: \n" + instrucaoUsuario);
+    return database.executar(instrucaoUsuario);
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-function cadastrar(nome, email, senha) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, fkEmpresa);
+function cadastrar(nome, email, senha, estado, cidade, bairro) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, estado, cidade, bairro);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
-    var instrucaoSql = `
+    var instrucaoUsuario = `
         INSERT INTO usuario (nome, email, senha) VALUES ('${nome}', '${email}', '${senha}');
     `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
-}
+    console.log("Executando a instrução SQL: \n" + instrucaoUsuario);
+    return database.executar(instrucaoUsuario)
+
+    .then(function (resultado) {
+         console.log("RESULTADO INSERT USUARIO:", resultado);
+
+            // pega o id do usuário criado e insere os dados da tabela endereço
+            var idUsuario = resultado.insertId;
+
+            var instrucaoEndereco = `
+                INSERT INTO endereco (estado, cidade, bairro, fk_usuario)
+                VALUES ('${estado}', '${cidade}', '${bairro}', ${idUsuario});
+            `;
+
+            return database.executar(instrucaoEndereco);
+        });
+        }
 
 module.exports = {
     autenticar,
